2e6a7e92708dd0da1a7d9d34f3e32d9d6b16e51f
Dylan Fosse Previs <p26dfosseprevis@fairfieldprep.org>
2024-04-29 11:55:56 -0400

    im on my wat to eat your skin

--- Sunstone-Medow.py
+++ Sunstone-Medow.py
@@ -1,18 +1,30 @@
 import timechild
 import discord
+#from discord_slash import SlashCommand, SlashContext
 from discord.ext import commands
 
 bot = commands.Bot()
-TOKEN = "nice try"
+TOKEN = "MTIzMTk3MzM2MzQwNTI5MTUzMA.G_LXsm.kG0pSJDm_58qBDLq-8uWtGwW7KvIqFlviEwmSU"
 
 
-@bot.slash_command(name="ping",description="returns pong")
-async def first_slash(ctx): 
-    await ctx.respond("Pong")
+@bot.slash_command(name="start",description="creates a farm")
+async def start(ctx): 
+    savefile = open(f"{ctx.author.name}.txt")
+    savefile.write()
+    print(ctx.author.name)
+    await ctx.respond(f"Hey {ctx.author.name} your farm is all ready to go")
     
+@bot.slash_command(name="view",description="Shows you your farm and inventory")
+async def view(ctx): 
+    print(ctx.author)
+    await ctx.respond(f"Hey {ctx.author} your farm is all ready to go")    
+    
+    
+    
+@bot.slash_command(name="buy",description="buys crop seeds")    
 async def buy(ctx, plant_id: int, row: int,colom: str,name="Buy",description="buys the plant with the id given"):
     
-  await ctx.respond()    
+  await ctx.respond("done")    
 
 print("Everything is working")
 bot.run(TOKEN)
