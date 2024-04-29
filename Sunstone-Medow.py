731b33cbe9fbe74c15b1543a918c524af316330d
Dylan Fosse Previs <p26dfosseprevis@fairfieldprep.org>
2024-04-29 12:44:49 -0400

    might as well

--- Sunstone-Medow.py
+++ Sunstone-Medow.py
@@ -9,20 +9,38 @@ TOKEN = ""
 
 @bot.slash_command(name="start",description="creates a farm")
 async def start(ctx): 
-    savefile = open(f"{ctx.author.name}.txt")
-    savefile.write()
+    savefile = open(f"savedata/{ctx.author.name}.txt","w")
+    savefile.write("1000")
+    savefile.write("0")
+    savefile.write([])
+    savefile.write([["dirt",timechild.today_date()],["dirt",timechild.today_date()],["dirt",timechild.today_date()]])
+    savefile.write([["dirt",timechild.today_date()],["dirt",timechild.today_date()],["dirt",timechild.today_date()]])
+    savefile.write([["dirt",timechild.today_date()],["dirt",timechild.today_date()],["dirt",timechild.today_date()]])
+    savefile.close()
     print(ctx.author.name)
     await ctx.respond(f"Hey {ctx.author.name} your farm is all ready to go")
     
 @bot.slash_command(name="view",description="Shows you your farm and inventory")
 async def view(ctx): 
-    print(ctx.author)
-    await ctx.respond(f"Hey {ctx.author} your farm is all ready to go")    
+    savefile = open(f"savedata/{ctx.author.name}.txt","r")
     
+    await ctx.respond(f"""You have {savefile.readline()} gold
+    Your fishing rod is level {savefile.readline()}
+    Your inventory looks like this {savefile.readline()}
+    And this is your farm:
+    {savefile.readline()}
+    {savefile.readline()}
+    {savefile.readline()}""")  
+    savefile.close()  
+    
+@bot.slash_command(name="shop",description="Shows the shop")
+async def shop(ctx): 
+
+    await ctx.respond(f"Hey {ctx.author} your farm is all ready to go")       
     
     
 @bot.slash_command(name="buy",description="buys crop seeds")    
-async def buy(ctx, plant_id: int, row: int,colom: str,name="Buy",description="buys the plant with the id given"):
+async def buy(ctx, plant_id: int, row: int,colom: str):
     
   await ctx.respond("done")    
 
