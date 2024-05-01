eab7e5850fcdcb06e6aa316d82915c09d8fd63fb
Dylan Fosse Previs <p26dfosseprevis@fairfieldprep.org>
2024-05-01 09:14:46 -0400

    dddd

--- Sunstone-Medow.py
+++ Sunstone-Medow.py
@@ -1,21 +1,22 @@
 import timechild
+import extrafun
 import discord
 #from discord_slash import SlashCommand, SlashContext
 from discord.ext import commands
 
 bot = commands.Bot()
-TOKEN = ""
+TOKEN = "MTIzMTk3MzM2MzQwNTI5MTUzMA.GTyMmd.fE8QxfEB5BY9i-fWUvGXnxTY46Rio3HTblVRlk"
 
 
 @bot.slash_command(name="start",description="creates a farm")
 async def start(ctx): 
     savefile = open(f"savedata/{ctx.author.name}.txt","w")
-    savefile.write("1000")
-    savefile.write("0")
-    savefile.write([])
-    savefile.write([["dirt",timechild.today_date()],["dirt",timechild.today_date()],["dirt",timechild.today_date()]])
-    savefile.write([["dirt",timechild.today_date()],["dirt",timechild.today_date()],["dirt",timechild.today_date()]])
-    savefile.write([["dirt",timechild.today_date()],["dirt",timechild.today_date()],["dirt",timechild.today_date()]])
+    savefile.write("1000\n")
+    savefile.write("0\n")
+    savefile.write(str([])+"\n")
+    savefile.write(str([["dirt",timechild.today_date()],["dirt",timechild.today_date()],["dirt",timechild.today_date()]])+"\n")
+    savefile.write(str([["dirt",timechild.today_date()],["dirt",timechild.today_date()],["dirt",timechild.today_date()]])+"\n")
+    savefile.write(str([["dirt",timechild.today_date()],["dirt",timechild.today_date()],["dirt",timechild.today_date()]])+"\n")
     savefile.close()
     print(ctx.author.name)
     await ctx.respond(f"Hey {ctx.author.name} your farm is all ready to go")
@@ -24,25 +25,30 @@ async def start(ctx):
 async def view(ctx): 
     savefile = open(f"savedata/{ctx.author.name}.txt","r")
     
-    await ctx.respond(f"""You have {savefile.readline()} gold
-    Your fishing rod is level {savefile.readline()}
-    Your inventory looks like this {savefile.readline()}
+    await ctx.respond(f"""You have {extrafun.remove_newline(savefile.readline())} gold
+    Your fishing rod is level {extrafun.remove_newline(savefile.readline())}
+    Your inventory looks like this {extrafun.remove_newline(savefile.readline())}
     And this is your farm:
-    {savefile.readline()}
-    {savefile.readline()}
-    {savefile.readline()}""")  
+    {extrafun.remove_newline(savefile.readline())}
+    {extrafun.remove_newline(savefile.readline())}
+    {extrafun.remove_newline(savefile.readline())}""")  
     savefile.close()  
     
 @bot.slash_command(name="shop",description="Shows the shop")
 async def shop(ctx): 
-
-    await ctx.respond(f"Hey {ctx.author} your farm is all ready to go")       
+    plants = open("plants.txt","r")
+    await ctx.respond(f"""{plants.readlines()}""")
+    plants.close()       
     
     
 @bot.slash_command(name="buy",description="buys crop seeds")    
-async def buy(ctx, plant_id: int, row: int,colom: str):
+async def buy(ctx, plant_id: int, spot_in_farm: str):
+    if spot_in_farm != "" OR :
     
-  await ctx.respond("done")    
+    await ctx.respond("done")    
+
+
+
 
 print("Everything is working")
 bot.run(TOKEN)
---
+++ extrafun.py
@@ -0,0 +1,3 @@
+def remove_newline(string):
+    """Remove the newline character from the end of a string."""
+    return string.rstrip('\n')
