153b9c242bd11717cf9bdbce9c971094cd5703d1
p26dfosseprevis <144148217+p26dfosseprevis@users.noreply.github.com>
2024-05-04 14:56:33 -0400

    added buy function

--- Sunstone-Medow.py
+++ Sunstone-Medow.py
@@ -1,15 +1,13 @@
 import timechild
 import extrafun
-import discord
-#from discord_slash import SlashCommand, SlashContext
 from discord.ext import commands
 
 bot = commands.Bot()
-TOKEN = "" #remember to add a token here 
+TOKEN = "MTIzMTk3MzM2MzQwNTI5MTUzMA.GGwtX3.1bnEAY9Es9x1vIUrqLrHedreATVbs-FMnp7mwM" #remember to add a token here 
+
 spots_farm = {"a1":4,"a2":5,"a3":6,"b1":7,"b2":8,"b3":9,"b4":10,"b5":11,"b6":12,}
-
-
-
+crop_sell = {"dirt":0,"carrot":50,"potato":400,"parsnip":150,"corn":0,}
+crop_emo={}
 @bot.slash_command(name="start",description="creates a farm")
 async def start(ctx): 
     savefile = open(f"savedata/{ctx.author.name}.txt","w")
@@ -30,9 +28,9 @@ async def view(ctx):
     Your fishing rod is level {extrafun.remove_newline(savefile.readline())}
     Your inventory looks like this {extrafun.remove_newline(savefile.readline())}
     And this is your farm:
-    {extrafun.remove_newline(savefile.readline())}{extrafun.remove_newline(savefile.readline())}{extrafun.remove_newline(savefile.readline())}
-    {extrafun.remove_newline(savefile.readline())}{extrafun.remove_newline(savefile.readline())}{extrafun.remove_newline(savefile.readline())}
-    {extrafun.remove_newline(savefile.readline())}{extrafun.remove_newline(savefile.readline())}{extrafun.remove_newline(savefile.readline())}""")  
+    {extrafun.remove_newline(savefile.readline())} {extrafun.remove_newline(savefile.readline())} {extrafun.remove_newline(savefile.readline())}
+    {extrafun.remove_newline(savefile.readline())} {extrafun.remove_newline(savefile.readline())} {extrafun.remove_newline(savefile.readline())}
+    {extrafun.remove_newline(savefile.readline())} {extrafun.remove_newline(savefile.readline())} {extrafun.remove_newline(savefile.readline())}""")  
     savefile.close()  
     
 @bot.slash_command(name="shop",description="Shows the shop")
@@ -45,28 +43,33 @@ async def shop(ctx):
 @bot.slash_command(name="buy",description="buys crop seeds")    
 async def buy(ctx, plant_id: int, spot_in_farm: str):
     savedata = f"savedata/{ctx.author.name}.txt"
-    print("got")
     if spot_in_farm not in ["a1","a2","a3","b1","b2","b3","c1","c2","c3",] :
-        await ctx.respond("thats not a valid location on the farm")
+        await ctx.respond("thats not a valid location on the farm \n try something like a1 or b3")
         
-    elif int(extrafun.get_line_in_file(savedata,1)) < int(extrafun.get_line_in_file("plants.txt",id)[1]):
-        print("oh nose")
+    elif int(extrafun.get_line_in_file(savedata,1)) < int((extrafun.get_line_in_file("plants.txt",plant_id))[1]):
         await ctx.respond("you don't have enuf money to buy that")
         
     else:
-        print("good")
-        new_crop_def = extrafun.get_line_in_file("plants.txt",id)  
-        new_crop = [new_crop_def[0],timechild.add_times(timechild.today_date(),new_crop[3])]
-        print("got")
-        extrafun.overwrite_line(savedata,spots_farm[id])
-        print("got")
-        await ctx.respond(f"done bought a {new_crop_def[0]} and planted it on tile {spot_in_farm}") 
-        print("you win")   
+        new_crop_def = extrafun.get_line_in_file("plants.txt",plant_id)  
+        new_crop = [new_crop_def[0],timechild.add_times(timechild.today_date(),new_crop_def[3])]
+        extrafun.overwrite_line(savedata,spots_farm[spot_in_farm],new_crop)
+        extrafun.overwrite_line(savedata,1,int(extrafun.get_line_in_file(savedata,1)) - int((extrafun.get_line_in_file("plants.txt",plant_id))[1]))
+        await ctx.respond(f"done bought a {new_crop_def[0]} and planted it on tile {spot_in_farm} for {new_crop_def[1]} \nyou now have {extrafun.get_line_in_file(savedata,1)} gold")   
 
 
 @bot.slash_command(name="sell",description="sells a crop in your farm")    
 async def buy(ctx, spot_in_farm: str):
-    await ctx.respond(f"sold")
-
+    savedata = f"savedata/{ctx.author.name}.txt"
+    if spot_in_farm not in ["a1","a2","a3","b1","b2","b3","c1","c2","c3",] :
+        await ctx.respond("thats not a valid location on the farm \n try something like a1 or b3")
+    else:
+        crop_data = extrafun.get_line_in_file(savedata,spots_farm[spot_in_farm])
+        if timechild.is_larger(crop_data[1],timechild.today_date):
+            print("good")
+            extrafun.overwrite_line(savedata,spots_farm[spot_in_farm],str(["dirt",timechild.today_date()]))
+            extrafun.overwrite_line(savedata,1,int(extrafun.get_line_in_file(savedata,1)) + crop_sell[str(crop_data[0])])
+            await ctx.respond(f"you sold a {crop_data[0]} for {crop_sell[str(crop_data[0])]} gold")
+        else:
+            await ctx.respond("sorry thats not done growing yet")
 print("Everything is working")
 bot.run(TOKEN)
--- extrafun.py
+++ extrafun.py
@@ -1,7 +1,7 @@
 import json
 
 def remove_newline(string):
-    """Remove the newline character from the end of a string."""
+    """Remove a newline character from the end of a string."""
     return string.rstrip('\n')
 
 def get_line_in_file(file_path, line_number):
@@ -10,17 +10,21 @@ def get_line_in_file(file_path, line_number):
         for current_line_number, line in enumerate(file, start=1):
             if current_line_number == line_number:
                 return json.loads(line.strip()) 
-    print("error out of range")
+    print(f"error {line_number} out of range in {file_path}")
     return None 
 
 def overwrite_line(file_path, line_number, new_content):
     with open(file_path, 'r') as file:
-            lines = file.readlines()
+        lines = file.readlines()
 
     if line_number < 1 or line_number > len(lines):
         print(f"Line number '{line_number}' is out of range.")
         return
-    lines[line_number - 1] = new_content + '\n'
+    new_content_str = str(new_content)
+    
+    lines[line_number - 1] = new_content_str + '\n'
+    
     with open(file_path, 'w') as file:
         file.writelines(lines)
+        
     print(f"Line {line_number} overwritten successfully.")
